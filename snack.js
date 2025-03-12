b4c285fb2b3523f914ddc697983132c061c80cb4
Marco Mechini <mechinimarco35@gmail.com>
2025-03-12 11:22:55 +0100

    .

--- snack.js
+++ snack.js
@@ -1,39 +1,58 @@
 // question 1 
 
-hamburger.name = 'Double Cheese Burger'; weight = 500
-secondBurger.name = 'Double Cheese Burger'; weight = 500
+// hamburger.name = 'Double Cheese Burger'; weight = 500
+// secondBurger.name = 'Double Cheese Burger'; weight = 500
 // gli oggetti creati in memoria sono solo 1 che puntano alla stessa cella di memoria
 
 
-// question 2
+// question 2 CORRETTO
 
-hamburger.ingredients = 'Salad'
-secondBurger.ingredients = 'Salad'
-    // sono stati creati 2 oggetti ma solo superficialmente
+// hamburger.ingredients = 'Salad'
+// secondBurger.ingredients = 'Salad'
+// sono stati creati 3 oggetti PERCHE HANNO L'ARRAY IN COMUNE
 
 
-// question 3
+// question 3 CORRETTO
 
-    sono stati creati 3 oggetti in memoria
+// sono stati creati 9 oggetti in memoria(3 secondBurger, 3 restaurant, 3 maker)
 
 
 // question 4
 
-    per clonare l'oggetti chef il miglior metodo è spread operator perchè fa una copia superficiale e si porta dietro le funzioni
+// per clonare l'oggetti chef il miglior metodo è spread operator perchè fa una copia superficiale e si porta dietro le funzioni
 
-    mentre per clonare restaurant il migliore è structuredClone per mantenere il frmato della data
+// mentre per clonare restaurant il migliore è structuredClone per mantenere il frmato della data
 
 
-    // question 5 WIP
+// question 5 CORRETTO
 
-    in memoria sono salvati 3 oggetti
+// in memoria sono salvati 5 oggetti
 
-    Anonymous Chef
-    chef Hyur
+// chef Hyur
+// chef Hyur
 
-    Hyur's Burgers
-    Hyur's II
+// Hyur's II
+// Hyur's II
 
 // question 6
 
-il miglior metodo è fare spread operator su chef, restaurant e PaymentAddress, in modo da non avere il riferiemnto in memoria e poter utilizzare le funzioni
+// il miglior metodo è fare spread operator su chef, restaurant e Address, in modo da non avere il riferiemnto in memoria e poter utilizzare le funzioni
+
+
+// question 7
+
+function deepCopy(oby) {
+    if (typrof obj !== 'object') {
+        return obj
+    }
+    const copy = {}
+    for (const key in obj) {
+        const value = obj[key];
+        if (typeof value !== 'object') {
+            copi[key] = value;
+        } else {
+            copy[key] = deepCopy(value);
+        }
+    }
+    return copy
+}
