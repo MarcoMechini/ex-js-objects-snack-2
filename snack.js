c510e6c456de1afd42179d7c0a478c280d13c077
Marco Mechini <mechinimarco35@gmail.com>
2025-03-12 10:37:46 +0100

    question 4 / 5 / 6

--- snack.js
+++ snack.js
@@ -9,16 +9,31 @@ secondBurger.name = 'Double Cheese Burger'; weight = 500
 
 hamburger.ingredients = 'Salad'
 secondBurger.ingredients = 'Salad'
-// sono stati creati 2 oggetti ma solo superficialmente
+    // sono stati creati 2 oggetti ma solo superficialmente
 
 
 // question 3
 
-sono stati creati 3 oggetti in memoria
+    sono stati creati 3 oggetti in memoria
 
 
 // question 4
 
-per clonare l'oggetti chef il miglior metodo è spread operator perchè fa una copia superficiale e si porta dietro le funzioni
+    per clonare l'oggetti chef il miglior metodo è spread operator perchè fa una copia superficiale e si porta dietro le funzioni
 
-mentre per clonare restaurant il migliore è structuredClone per mantenere il frmato della data
+    mentre per clonare restaurant il migliore è structuredClone per mantenere il frmato della data
+
+
+    // question 5 WIP
+
+    in memoria sono salvati 3 oggetti
+
+    Anonymous Chef
+    chef Hyur
+
+    Hyur's Burgers
+    Hyur's II
+
+// question 6
+
+il miglior metodo è fare spread operator su chef, restaurant e PaymentAddress, in modo da non avere il riferiemnto in memoria e poter utilizzare le funzioni
